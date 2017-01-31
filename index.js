43b5ae568c4d7bbf5d3abd53ee4a03baf24d8b9b
Mark Wubben <mark@novemberborn.net>
2017-01-31 18:02:18 +0000

    Remove Babel

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
-import { execFileSync } from 'child_process'
-import { readFileSync, statSync } from 'fs'
-import { dirname, join, resolve } from 'path'
+'use strict'
 
-import md5hex from 'md5-hex'
+const execFileSync = require('child_process').execFileSync
+const fs = require('fs')
+const path = require('path')
 
-const PACKAGE_DIR = resolve(__dirname, '..')
+const md5hex = require('md5-hex')
+
+const PACKAGE_DIR = path.resolve(__dirname)
 
 function tryReadFileSync (file) {
   try {
-    return readFileSync(file)
+    return fs.readFileSync(file)
   } catch (err) {
     return null
   }
@@ -21,7 +23,7 @@ const git = {
     const m = /^ref: (.+)$/.exec(head.toString('utf8').trim())
     if (!m) return null
 
-    return tryReadFileSync(join(dir, '.git', m[1]))
+    return tryReadFileSync(path.join(dir, '.git', m[1]))
   },
 
   tryGetDiff (dir) {
@@ -36,7 +38,7 @@ const git = {
         env: Object.assign({}, process.env, {
           // Force the GIT_DIR to prevent git from diffing a parent repository
           // in case the directory isn't actually a repository.
-          GIT_DIR: join(dir, '.git')
+          GIT_DIR: path.join(dir, '.git')
         }),
         // Ignore stderr.
         stdio: ['ignore', 'pipe', 'ignore']
@@ -47,18 +49,20 @@ const git = {
   }
 }
 
-function addPackageData (inputs, path) {
-  const dir = statSync(path).isDirectory() ? path : dirname(path)
+function addPackageData (inputs, pkgPath) {
+  const dir = fs.statSync(pkgPath).isDirectory()
+    ? pkgPath
+    : path.dirname(pkgPath)
   inputs.push(dir)
 
-  const pkg = readFileSync(join(dir, 'package.json'))
+  const pkg = fs.readFileSync(path.join(dir, 'package.json'))
   inputs.push(pkg)
 
-  const head = tryReadFileSync(join(dir, '.git', 'HEAD'))
+  const head = tryReadFileSync(path.join(dir, '.git', 'HEAD'))
   if (head) {
     inputs.push(head)
 
-    const packed = tryReadFileSync(join(dir, '.git', 'packed-refs'))
+    const packed = tryReadFileSync(path.join(dir, '.git', 'packed-refs'))
     if (packed) inputs.push(packed)
 
     const ref = git.tryGetRef(dir, head)
@@ -83,15 +87,15 @@ function computeHash (paths, pepper, salt) {
     }
   }
 
-  for (let i = 0; i < paths.length; i++) {
-    addPackageData(inputs, paths[i])
+  for (const pkgPath of paths) {
+    addPackageData(inputs, pkgPath)
   }
 
   return md5hex(inputs)
 }
 
 let ownHash = null
-export function sync (paths, salt) {
+function sync (paths, salt) {
   if (!ownHash) {
     // Memoize the hash for package-hash itself.
     ownHash = new Buffer(computeHash([PACKAGE_DIR]), 'hex')
@@ -103,9 +107,8 @@ export function sync (paths, salt) {
     return ownHash.toString('hex')
   }
 
-  if (Array.isArray(paths)) {
-    return computeHash(paths, ownHash, salt)
-  } else {
-    return computeHash([paths], ownHash, salt)
-  }
+  return Array.isArray(paths)
+    ? computeHash(paths, ownHash, salt)
+    : computeHash([paths], ownHash, salt)
 }
+exports.sync = sync
