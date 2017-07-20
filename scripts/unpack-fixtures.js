f9f4db569fedb9e02935dd698ae8352db66ee8b2
Mark Wubben <mark@novemberborn.net>
2017-07-20 15:48:07 +0100

    Update tar implementation

--- scripts/unpack-fixtures.js
+++ scripts/unpack-fixtures.js
@@ -1,22 +1,19 @@
 'use strict'
 
-const createReadStream = require('fs').createReadStream
-const readdirSync = require('fs').readdirSync
-const join = require('path').join
-const resolve = require('path').resolve
-
+const fs = require('fs')
+const path = require('path')
 const rimraf = require('rimraf')
 const tar = require('tar')
 
-const unpackedDir = resolve(__dirname, '..', 'test', 'fixtures', 'unpacked')
-const packedDir = resolve(__dirname, '..', 'test', 'fixtures', 'packed')
+const unpackedDir = path.resolve(__dirname, '..', 'test', 'fixtures', 'unpacked')
+const packedDir = path.resolve(__dirname, '..', 'test', 'fixtures', 'packed')
 
 rimraf.sync(unpackedDir)
+fs.mkdirSync(unpackedDir)
 
-const files = readdirSync(packedDir)
+fs.readdirSync(packedDir)
   .filter(name => /\.tar$/.test(name))
-  .map(name => join(packedDir, name))
-
-files.forEach(file => {
-  createReadStream(file).pipe(tar.Extract(unpackedDir))
-})
+  .map(name => path.join(packedDir, name))
+  .forEach(file => {
+    tar.extract({cwd: unpackedDir, file, sync: true, strict: true})
+  })
